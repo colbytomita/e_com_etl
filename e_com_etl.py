5d9e4d759eced1ac0612c2799074f7bf755fc5bf
Colby Tomita <cotomita808@gmail.com>
2024-07-13 17:17:13 -1000

    correctly authenticated and  extracted the e commerce datasets from kaggle

--- e_com_etl.py
+++ e_com_etl.py
@@ -2,7 +2,27 @@ import pandas as pd
 import json
 from datetime import datetime
 import s3fs
+import os
 from dotenv import load_dotenv
-import kaggle
 
 # Load environment variables
+load_dotenv()
+os.environ['KAGGLE_USERNAME'] = os.getenv('KAGGLE_USERNAME')
+os.environ['KAGGLE_KEY'] = os.getenv('KAGGLE_KEY')
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+
+# Load Kaggle API credentials
+api = KaggleApi()
+
+api.authenticate()
+
+# Download dataset
+api.dataset_download_files('thedevastator/unlock-profits-with-e-commerce-sales-data', path='data', unzip=True)
+
+# Load data
+df = pd.read_csv('data/Amazon Sale Report.csv')
+
+print(df.head())
